268b79ed8843456a695646f28224c6cc7aa0d2a6
Lani <ooluwalani@gmail.com>
2021-08-18 17:16:15 +0100

    copy api keys

--- src/views/Api/Api.vue
+++ src/views/Api/Api.vue
@@ -1,14 +1,31 @@
 <template>
 	<div class="dashboard-wrapper">
 		<home-header />
-		<div class="page-content">
+		<div class="loader" v-if="pageLoading">
+			<img src="@/assets/icons/page-loader.svg" alt="" />
+		</div>
+		<div v-else-if="!keys.length" class="page-wrapper">
+			<img src="@/assets/icons/api-generate.svg" alt="" />
+			<div class="text">
+				<h2 class="text-main">Get an API Key</h2>
+				<p class="text-sub">Click on the button to generate an API key</p>
+			</div>
+			<v-button class="config__btn__close" @click="getKey">
+				<div class="btn__content__wrapper">
+					<Loader v-if="loading" />
+					<span v-else class="text">Generate API Key</span>
+				</div>
+			</v-button>
+			<!-- <v-button class="submit" size="large" buttonType="primary">Generate API Key</v-button> -->
+		</div>
+		<div v-else class="page-content">
 			<h3 class="page-content__name">API Keys</h3>
 			<div class="page-content__body">
 				<h4 class="form-head">
 					Integration
 				</h4>
 
-				<div v-if="!keys.length">
+				<!-- <div v-if="!keys.length">
 					<div class="form-body">
 						<p class="label">API key</p>
 						<div class="key-section">
@@ -28,18 +45,21 @@
 							<div class="btn">Copy</div>
 						</div>
 					</div>
-				</div>
-				<template v-else>
+				</div> -->
+
+				<template>
 					<div v-for="(api, index) in keys" :key="index">
 						<div class="form-body">
 							<p class="label">API {{ api.mode }} key</p>
 							<div class="key-section">
 								<div class="key-group">
-									<p class="key-group__text">{{ api.key }}</p>
+									<p v-if="index === 0" class="key-group__text" ref="liveKey">{{ api.key }}</p>
+									<p v-if="index === 1" class="key-group__text" ref="testKey">{{ api.key }}</p>
 								</div>
-								<div class="btn">Copy</div>
+								<div v-if="index === 0" class="btn" ref="liveBtn" @click="copyLiveKey">Copy</div>
+								<div v-if="index === 1" class="btn" ref="testBtn" @click="copyTestKey">Copy</div>
 							</div>
-							<button v-if="index === 0" class="key-generate" @click="getKey">Generate key</button>
+							<button v-if="index === 0" class="key-generate" @click="getKey">Regenerate key</button>
 						</div>
 					</div>
 				</template>
@@ -51,18 +71,23 @@
 <script>
 import HomeHeader from '@/components/Header/home/Header';
 import { mapActions } from 'vuex';
+import VButton from '@/components/Button';
+import Loader from '@/components/Loader';
 
 export default {
 	name: 'Api',
 	data() {
 		return {
-			keys: []
+			keys: [],
+			loading: false,
+			pageLoading: true
 		};
 	},
 	components: {
-		HomeHeader
+		HomeHeader,
+		VButton,
+		Loader
 	},
-	computed: {},
 	async mounted() {
 		try {
 			const response = await this.fetchApiKeys();
@@ -92,6 +117,8 @@ export default {
 				message: 'Unable to retrieve Api keys',
 				showAlert: true
 			});
+		} finally {
+			this.pageLoading = false;
 		}
 	},
 	methods: {
@@ -103,6 +130,7 @@ export default {
 		}),
 
 		async getKey() {
+			this.loading = true;
 			console.log(this.keys);
 
 			if (!this.keys.length) {
@@ -114,26 +142,27 @@ export default {
 		},
 		async generateKeys() {
 			console.log('no existing key');
-			try {
-				const response = await this.generateApiKey();
-				const { status, statusText, data } = response;
-				if (status === 200 && statusText === 'OK') {
-					this.keys = data.keys[0];
-					console.log('generate -->', data.keys);
+			// try {
+			// 	const response = await this.generateApiKey();
+			// 	const { status, statusText, data } = response;
+			// 	if (status === 200 && statusText === 'OK') {
+			// 		this.keys = data.keys[0];
+			// 		console.log('generate -->', data.keys);
 
-					this.showAlert({
-						status: 'success',
-						message: 'Api Keys regenerated successfully',
-						showAlert: true
-					});
-				}
-			} catch (error) {
-				this.showAlert({
-					status: 'error',
-					message: 'Unable to regenerate Api keys',
-					showAlert: true
-				});
-			}
+			// 		this.showAlert({
+			// 			status: 'success',
+			// 			message: 'Api Keys regenerated successfully',
+			// 			showAlert: true
+			// 		});
+			// 	}
+			// } catch (error) {
+			// 	this.showAlert({
+			// 		status: 'error',
+			// 		message: 'Unable to regenerate Api keys',
+			// 		showAlert: true
+			// 	});
+			// }
+			this.loading = false;
 		},
 		async regenerateKeys() {
 			console.log('keys exist');
@@ -159,6 +188,45 @@ export default {
 					message: 'Unable to regenerate Api keys',
 					showAlert: true
 				});
+			}
+		},
+		copyLiveKey() {
+			const liveKey = this.$refs.liveKey[0].textContent;
+			const btn = this.$refs.liveBtn[0];
+			this.copyKey(liveKey, btn);
+		},
+
+		copyTestKey() {
+			const testKey = this.$refs.testKey[0].textContent;
+			const btn = this.$refs.testBtn[0];
+			this.copyKey(testKey, btn);
+		},
+
+		copyKey(selectedKey, copyBtn) {
+			if (selectedKey) {
+				navigator.clipboard
+					.writeText(selectedKey)
+					.then(() => {
+						if (copyBtn.innerText !== 'Copied!') {
+							const originalText = copyBtn.innerText;
+							copyBtn.innerText = 'Copied!';
+							setTimeout(() => {
+								copyBtn.innerText = originalText;
+							}, 1500);
+						}
+						this.showAlert({
+							status: 'success',
+							message: 'Api Key copied',
+							showAlert: true
+						});
+					})
+					.catch(() => {
+						this.showAlert({
+							status: 'error',
+							message: 'Unable to copy Api key',
+							showAlert: true
+						});
+					});
 			}
 		}
 	}
@@ -246,6 +314,34 @@ export default {
 			color: #3b48f7;
 			cursor: pointer;
 		}
+	}
+}
+.page-wrapper {
+	text-align: center;
+	margin-top: 9%;
+}
+.loader {
+	display: grid;
+	place-items: center;
+	min-height: 80vh;
+}
+.text {
+	margin-top: 60px;
+	margin-bottom: 32px;
+
+	&-main {
+		font-size: 32px;
+		line-height: 32px;
+		letter-spacing: -0.008em;
+		color: #333758;
+		margin-bottom: 12px;
+	}
+
+	&-sub {
+		font-size: 16px;
+		line-height: 24px;
+		letter-spacing: -0.002em;
+		color: #202f44;
 	}
 }
 </style>
