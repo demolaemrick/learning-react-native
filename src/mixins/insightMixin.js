9497321a39aadcb436185b31a4b3403c545f469f
Lani <ooluwalani@gmail.com>
2021-08-19 09:17:49 +0100

    remove spaces

--- src/mixins/insightMixin.js
+++ src/mixins/insightMixin.js
@@ -89,12 +89,12 @@ export default {
 			dislike: 'search_services/dislike'
 		}),
 		sortByDislike(data) {
-			data.sort(function (a, b) {
+			data.sort(function(a, b) {
 				return a.is_disliked - b.is_disliked;
 			});
 		},
 		sortByBookmarked(data) {
-			data.sort(function (a, b) {
+			data.sort(function(a, b) {
 				return b.is_bookmarked - a.is_bookmarked;
 			});
 		},
