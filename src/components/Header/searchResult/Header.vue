7733a8d5c30a6d242843a19a6efda7ff67099f93
Lani <ooluwalani@gmail.com>
2021-08-19 11:03:28 +0100

    added api link to headers

--- src/components/Header/searchResult/Header.vue
+++ src/components/Header/searchResult/Header.vue
@@ -21,6 +21,7 @@
 							<img src="@/assets/icons/carret-down.svg" svg-inline />
 						</template>
 						<template #dropdown-items>
+							<li class="dropdown__item" @click="$router.push({ name: 'ApiPortal' })">API Keys</li>
 							<li class="dropdown__item" @click="$router.push({ name: 'Bookmarks' })">Bookmarks</li>
 							<li
 								class="dropdown__item"
