60d3425e0da8f3327f296c9cf36f59c40c74f441
Lani <ooluwalani@gmail.com>
2021-08-30 10:58:54 +0100

    Removed white spaces

--- src/views/Insights/insights.js
+++ src/views/Insights/insights.js
@@ -384,7 +384,7 @@ export default {
 		}
 	},
 	watch: {
-		contactSearchQuery: debounce(function (newVal) {
+		contactSearchQuery: debounce(function(newVal) {
 			if (newVal) {
 				this.contactSearch(newVal);
 			} else {
@@ -392,7 +392,7 @@ export default {
 				this.contactFilter = null;
 			}
 		}, 600),
-		companySearchQuery: debounce(function (newVal) {
+		companySearchQuery: debounce(function(newVal) {
 			if (newVal) {
 				this.companySearch(newVal);
 			} else {
