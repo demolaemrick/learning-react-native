67b59e743e8529f91bd9ef1f0cfc4b72fe97c15d
Lani <ooluwalani@gmail.com>
2021-08-31 16:43:53 +0100

    remove white spaces

--- tests/unit/pages/Users.spec.js
+++ tests/unit/pages/Users.spec.js
@@ -469,7 +469,11 @@ describe('Users', () => {
 			}
 		});
 
-		const icon = wrapper.find('.table__wrapper').findAll('td').at(5).find('.dropdown__wrapper');
+		const icon = wrapper
+			.find('.table__wrapper')
+			.findAll('td')
+			.at(5)
+			.find('.dropdown__wrapper');
 		icon.trigger('click');
 		await wrapper.vm.$nextTick();
 	});
