a5be25793b7cfd813573ff180f8ef7dea09bf132
Lani <ooluwalani@gmail.com>
2021-09-07 11:43:02 +0100

    removed white spaces

--- src/views/Insights/insights.js
+++ src/views/Insights/insights.js
@@ -354,7 +354,7 @@ export default {
 		}
 	},
 	watch: {
-		contactSearchQuery: debounce(function (newVal) {
+		contactSearchQuery: debounce(function(newVal) {
 			if (newVal) {
 				this.contactSearch(newVal);
 			} else {
@@ -362,7 +362,7 @@ export default {
 				this.contactFilter = null;
 			}
 		}, 600),
-		companySearchQuery: debounce(function (newVal) {
+		companySearchQuery: debounce(function(newVal) {
 			if (newVal) {
 				this.companySearch(newVal);
 			} else {
