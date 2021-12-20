57c4d01971659b2d8f76da0033b7ee6f7e26b9f8
Lani <ooluwalani@gmail.com>
2021-12-20 10:00:10 +0100

    lint fixes

--- src/views/ContactResearch/contactResearch.js
+++ src/views/ContactResearch/contactResearch.js
@@ -392,7 +392,7 @@ export default {
 		}
 	},
 	watch: {
-		searchQuery: debounce(function (newVal) {
+		searchQuery: debounce(function(newVal) {
 			if (newVal) {
 				// this.searchPage({
 				// 	q: newVal
--- tests/unit/pages/Users.spec.js
+++ tests/unit/pages/Users.spec.js
@@ -540,7 +540,11 @@ describe('Users', () => {
 			}
 		});
 
-		const icon = wrapper.find('.table__wrapper').findAll('td').at(5).find('.dropdown__wrapper');
+		const icon = wrapper
+			.find('.table__wrapper')
+			.findAll('td')
+			.at(5)
+			.find('.dropdown__wrapper');
 		await icon.trigger('click');
 		await wrapper.vm.$nextTick();
 	});
