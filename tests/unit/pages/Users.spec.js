5ac76a07201f0ab9690c6763e8b32021b27c335e
Jerry Hogan <jerry@enyata.com>
2021-12-21 10:18:39 +0100

    modal not closing on cancle button click

--- tests/unit/pages/Users.spec.js
+++ tests/unit/pages/Users.spec.js
@@ -540,11 +540,7 @@ describe('Users', () => {
 			}
 		});
 
-		const icon = wrapper
-			.find('.table__wrapper')
-			.findAll('td')
-			.at(5)
-			.find('.dropdown__wrapper');
+		const icon = wrapper.find('.table__wrapper').findAll('td').at(5).find('.dropdown__wrapper');
 		await icon.trigger('click');
 		await wrapper.vm.$nextTick();
 	});
