0332b74db59accd15af7a9ce703ac599419e0577
Ademola Akindotuni <emricky17@gmail.com>
2022-08-25 16:30:21 +0100

    Round up emails found percentage to 1 decimal place

--- src/views/UniqueDataPlatform/unique-data-platform.js
+++ src/views/UniqueDataPlatform/unique-data-platform.js
@@ -209,13 +209,11 @@ export default {
 			getContactPageData: 'user/getContactPageData'
 		}),
 		emailsFound() {
-			const emailCount = `${this.totalEmails > this.totalContacts ? `${this.totalContacts}+` : this.totalEmails}/${
-				this.totalContacts
-			}`;
-			return emailCount;
+			let foundEmails = this.totalEmails > this.totalContacts ? `${this.totalContacts}+` : this.totalEmails;
+			return `${foundEmails} / ${this.totalContacts}`;
 		},
 		percentageOfEmailsFound() {
-			let percentage = (this.totalEmails / this.totalContacts) * 100;
+			let percentage = ((this.totalEmails / this.totalContacts) * 100).toFixed(1);
 			if (Number.isNaN(percentage)) {
 				percentage = 0;
 			}
