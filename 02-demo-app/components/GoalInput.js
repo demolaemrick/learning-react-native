10e832370b92f378eb584a7e9cd55c495002717a
Ademola Akindotuni <emricky17@gmail.com>
2022-09-11 16:02:59 +0100

    styling modal overlay

--- 02-demo-app/components/GoalInput.js
+++ 02-demo-app/components/GoalInput.js
@@ -12,19 +12,26 @@ function GoalInput(props) {
     props.onAddGoal(enteredGoalText);
     setEnteredGoalText("");
   }
-    return (
-      <Modal visible={props.visible} animationType="slide">
-        <View style={styles.inputContainer}>
-          <TextInput
-            style={styles.textInput}
-            placeholder="Your course goal!"
-            onChangeText={goalInputHandler}
-            value={enteredGoalText}
-          />
-          <Button title="Add Goal" onPress={addGoalHandler} />
+  return (
+    <Modal visible={props.visible} animationType="slide">
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your course goal!"
+          onChangeText={goalInputHandler}
+          value={enteredGoalText}
+        />
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button title="Add Goal" onPress={addGoalHandler} />
+          </View>
+          <View style={styles.button}>
+            <Button title="Cancel" />
+          </View>
         </View>
-      </Modal>
-    );
+      </View>
+    </Modal>
+  );
 }
 
 export default GoalInput;
@@ -32,18 +39,25 @@ export default GoalInput;
 const styles = StyleSheet.create({
   inputContainer: {
     flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-between",
+    justifyContent: "center",
     alignItems: "center",
     marginBottom: 24,
+    padding: 16,
     borderBottomWidth: 1,
     borderBottomColor: "#cccccc",
   },
   textInput: {
     borderWidth: 1,
     borderColor: "#cccccc",
-    width: "70%",
-    marginRight: 8,
+    width: "100%",
     padding: 8,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    marginTop: 16,
+  },
+  button: {
+    width: 100,
+    marginHorizontal: 8,
   },
 });
