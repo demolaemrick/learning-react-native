43551c7facf6e9812cf4ebbe72a8fd0d80f05906
Ademola Akindotuni <emricky17@gmail.com>
2022-09-12 11:29:58 +0100

    Adding shadow styling on ios

--- 03-guess-game-app/screens/StartGameScreen.js
+++ 03-guess-game-app/screens/StartGameScreen.js
@@ -1,12 +1,33 @@
-import { TextInput, View } from 'react-native';
-import PrimaryButton from '../components/PrimaryButton';
+import { TextInput, View, StyleSheet } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
-    return <View>
-        <TextInput />
-        <PrimaryButton>Reset</PrimaryButton>
-        <PrimaryButton>Confirm</PrimaryButton>
-  </View>;
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  );
 }
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: "#720636",
+    borderRadius: 8,
+    /* this only adds shadow to android */
+    elevation: 4,
+    /* adding shadow to ios */
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 10 },
+    /* controls how much the shadow expands on ios */
+    shadowRadius: 6,
+    /* the shadow's thickness on ios */
+    shadowOpacity: 0.25,
+  },
+});
