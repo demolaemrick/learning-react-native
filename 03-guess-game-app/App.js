668ab43df00a1c3776642bf4e2f2b20aa8d1354d
Ademola Akindotuni <emricky17@gmail.com>
2022-09-12 14:16:08 +0100

    add background image

--- 03-guess-game-app/App.js
+++ 03-guess-game-app/App.js
@@ -1,12 +1,20 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, ImageBackground } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 
 import StartGameScreen from "./screens/StartGameScreen";
+import background from "./assets/images/background.png";
 
 export default function App() {
   return (
     <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.rootScreen}>
-      <StartGameScreen />
+      <ImageBackground
+        source={background}
+        resizeMode="cover"
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}
+      >
+        <StartGameScreen />
+      </ImageBackground>
     </LinearGradient>
   );
 }
@@ -14,5 +22,8 @@ export default function App() {
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
-   },
+  },
+  backgroundImage: {
+    opacity: 0.15,
+  },
 });
