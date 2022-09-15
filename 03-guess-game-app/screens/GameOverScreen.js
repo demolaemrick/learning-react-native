33c6d8dbec7e7ea1bc096aa748d5def25606a85e
Ademola Akindotuni <emricky17@gmail.com>
2022-09-15 21:55:20 +0100

    Adding a foreground image

--- 03-guess-game-app/screens/GameOverScreen.js
+++ 03-guess-game-app/screens/GameOverScreen.js
@@ -1,7 +1,41 @@
-import { Text } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
+import Title from '../components/ui/Title';
+
+import Colors from '../constants/colors';
+import success from '../assets/images/success.png';
 
 function GameOverScreen() {
-  return <Text>GameOverScreen</Text>;
+  return (
+    <View style={styles.rootContainer}>
+      <Title>GAME OVER</Title>
+      <View style={styles.imageContainer}>
+        <Image style={styles.image} source={success} />
+      </View>
+      <Text>Your phone needed X rounds to guess the number Y.</Text>
+    </View>
+  );
 }
 
 export default GameOverScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    padding: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 1500,
+    borderWidth: 3,
+    borderColor: Colors.primary800,
+    overflow: 'hidden',
+    margin: 36,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+});
