695839d1b0f229dfac7de9c51282ef63ec16ba51
Ademola Akindotuni <emricky17@gmail.com>
2022-09-15 15:23:54 +0100

    adding custom fonts

--- 03-guess-game-app/App.js
+++ 03-guess-game-app/App.js
@@ -1,6 +1,9 @@
 import { useState } from 'react';
 import { StyleSheet, ImageBackground, SafeAreaView } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
+import { useFonts } from 'expo-font';
+/* this package has been deprecated use expo */
+import AppLoading from 'expo-app-loading';
 
 import StartGameScreen from './screens/StartGameScreen';
 import GameScreen from './screens/GameScreen';
@@ -12,6 +15,14 @@ export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [gameIsOver, setGameIsOver] = useState(true);
 
+  const [fontsLoaded] = useFonts({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
   function pickedNumberHandler(pickedNumber) {
     setUserNumber(pickedNumber);
     setGameIsOver(false);
--- 03-guess-game-app/components/game/NumberContainer.js
+++ 03-guess-game-app/components/game/NumberContainer.js
@@ -2,30 +2,29 @@ import { View, Text, StyleSheet } from 'react-native';
 
 import Colors from '../../constants/colors';
 
-function NumberContainer({children}) {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.numberText}>{children}</Text>
-      </View>
-    );
+function NumberContainer({ children }) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.numberText}>{children}</Text>
+    </View>
+  );
 }
 
 export default NumberContainer;
 
 const styles = StyleSheet.create({
-    container: {
-        borderWidth: 4,
-        borderColor: Colors.accent500,
-        padding: 24,
-        margin: 24,
-        borderRadius: 8,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    numberText: {
-        color: Colors.accent500,
-        fontSize: 36,
-        fontWeight: 'bold'
-
-    }
-})
+  container: {
+    borderWidth: 4,
+    borderColor: Colors.accent500,
+    padding: 24,
+    margin: 24,
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  numberText: {
+    fontFamily: 'open-sans-bold',
+    color: Colors.accent500,
+    fontSize: 36,
+  },
+});
