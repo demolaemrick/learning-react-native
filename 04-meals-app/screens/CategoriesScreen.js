e93f25c2744af1ce99240e8749d4a87d00b22e68
Ademola Akindotuni <emricky17@gmail.com>
2022-10-08 16:00:12 +0100

    getting started

---
+++ 04-meals-app/screens/CategoriesScreen.js
@@ -0,0 +1,20 @@
+import { FlatList } from 'react-native';
+import CategoryGridTile from '../components/CategoryGridTile';
+
+import { CATEGORIES } from '../data/dummy-data';
+
+const renderCategoryItem = (itemData) => (
+  <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
+);
+
+const CategoriesScreen = () => {
+  return (
+    <FlatList
+      data={CATEGORIES}
+      keyExtractor={(item) => item.id}
+      renderItem={renderCategoryItem}
+    />
+  );
+};
+
+export default CategoriesScreen;
